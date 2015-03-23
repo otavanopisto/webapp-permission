1f0b531f449817544904fd2728e96af47dfdcfcb
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-03-23 10:34:17 +0200

    - Initial commit of security package

---
+++ src/main/java/fi/otavanopisto/security/ContextReference.java
@@ -0,0 +1,9 @@
+package fi.otavanopisto.security;
+
+/**
+ * Marker interface for Permission Context objects
+ * 
+ * @author antti.viljakainen
+ */
+public interface ContextReference {
+}
---
+++ src/main/java/fi/otavanopisto/security/User.java
@@ -0,0 +1,5 @@
+package fi.otavanopisto.security;
+
+public interface User {
+
+}
